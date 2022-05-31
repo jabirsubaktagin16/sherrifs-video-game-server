26802cde7cdc0fa2fcba81fe01d68fbf697ff374
asjV2 <ahmad.subaktagin16@gmail.com>
2022-05-31 20:01:48 +0600

    Single Game Details Handled

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -8,6 +9,21 @@ require("dotenv").config();
 //Middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qo60e.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -23,6 +39,25 @@ const run = async () => {
       .db("sherrifs-video-game")
       .collection("reviews");
     const gameCollection = client.db("sherrifs-video-game").collection("games");
+    const userCollection = client.db("sherrifs-video-game").collection("users");
+
+    // Token Generate and Store User Email in Database
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1d" }
+      );
+      res.send({ result, token });
+    });
 
     // GET All Games
     app.get("/games", async (req, res) => {
@@ -37,6 +72,14 @@ const run = async () => {
       const query = {};
       const cursor = reviewCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Get Single Game by Id
+    app.get("/game/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await gameCollection.findOne(query);
       res.send(result);
     });
   } finally {
